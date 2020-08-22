f23974a8b9ca738dd0a0ab4ff76fb9d6913cc389
Brayan Obispo <segunda.bo@gmail.com>
2020-08-21 21:13:20 -0500

    Updates

---
+++ precache-manifest.d85a5d35f15bdde4e05c1d4efcefe25a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b2e6001ec83fe40dd6f28dd370114e35",
+    "url": "/control-de-presupuesto/index.html"
+  },
+  {
+    "revision": "2b0cb994838601fa33a2",
+    "url": "/control-de-presupuesto/static/css/main.dc1663a9.chunk.css"
+  },
+  {
+    "revision": "f52147d76d32dcc8035f",
+    "url": "/control-de-presupuesto/static/js/2.8bd2830c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/control-de-presupuesto/static/js/2.8bd2830c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2b0cb994838601fa33a2",
+    "url": "/control-de-presupuesto/static/js/main.8902ff92.chunk.js"
+  },
+  {
+    "revision": "b07f18867b3f9a82bf21",
+    "url": "/control-de-presupuesto/static/js/runtime-main.de6261d0.js"
+  }
+]);
